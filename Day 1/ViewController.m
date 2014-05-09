4f9abb028cd64c15129820f5297a8516b7a00a12
Austin Campbell <austinscampbell@gmail.com>
2014-05-08 18:39:36 -0700

    Finish homework for day one.

--- Day 1/ViewController.m
+++ Day 1/ViewController.m
@@ -26,4 +26,40 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)logNameToConsole:(id)sender
+
+{
+    UIButton *theButton = sender;
+  NSLog(@"Button Title Is: %@", theButton.titleLabel.text);
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+- (IBAction)logFirstNameToConsole:(id)sender
+{
+    NSLog(@"Austin");
+    
+    UIColor *greenColor = [UIColor greenColor];
+    self.view.backgroundColor = greenColor;
+    
+}
+
+- (IBAction)toggle:(id)sender {
+    NSLog(@"The switch is switched");
+}
+
+
+- (IBAction)slider:(id)sender
+{
+    UISlider *slider = sender;
+    
+    if (slider.value > .5) {
+        
+        NSLog(@"Slider Value is: %f", slider.value);
+    }
+    
+}
+
+
 @end
